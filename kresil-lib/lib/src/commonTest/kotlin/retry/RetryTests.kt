dcb3a366acd821594d33d1ff43df0cc00167268b
Francisco Engenheiro <a49428@alunos.isel.pt>
2024-04-19 14:23:27 +0100

    🧪 test(retry): tests delay duration with test scheduler virtual time

--- kresil-lib/lib/src/commonTest/kotlin/retry/RetryTests.kt
+++ kresil-lib/lib/src/commonTest/kotlin/retry/RetryTests.kt
@@ -8,28 +8,100 @@ import io.mockative.coEvery
 import io.mockative.coVerify
 import io.mockative.mock
 import io.mockative.once
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.currentTime
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.withContext
-import kresil.duration.seconds
 import kresil.retry.Retry
 import kresil.retry.config.RetryConfig
 import kresil.retry.retryConfig
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.fail
-import kotlin.time.Duration
-import kotlin.time.measureTime
+import kotlin.time.Duration.Companion.seconds
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class RetryTests {
 
     @Mock
     val remoteService = mock(classOf<RemoteService>())
 
+    // TODO: add repeated tests using randomTo (infix) function
     @Test
-    fun executeSuspendFunctionWithRetryConfig() = runTest {
+    fun executeSuspendFunctionWithRetry() = executeSuspendFunctionWithRetry(false)
 
+    @Test
+    fun decoratedSuspendFunctionWithRetry() = executeSuspendFunctionWithRetry(true)
+
+    private fun executeSuspendFunctionWithRetry(isDecorated: Boolean) = runTest {
+
+        // given: a retry configuration
+        val maxAttempts = 3
+        val delayDuration = 3.seconds
+        val config: RetryConfig = retryConfig {
+            maxAttempts(maxAttempts)  // includes the first non-retry attempt
+            retryIf { it is WebServiceException }
+            delay(delayDuration)
+        }
+
+        // and: a retry instance
+        val retry = Retry(config)
+
+        // and: a remote service that always throws an exception
+        coEvery { remoteService.suspendCall() }
+            .throws(WebServiceException("BAM!"))
+
+        launch {
+            try {
+                when (isDecorated) {
+                    true -> {
+                        // and: a decorated suspend function
+                        val decorated = retry.decorateSuspendFunction {
+                            remoteService.suspendCall()
+                        }
+
+                        // when: a suspend function is executed with the retry instance
+                        decorated()
+                    }
+
+                    false -> {
+                        // when: a suspend function is executed with the retry instance
+                        retry.executeSuspendFunction {
+                            remoteService.suspendCall()
+                        }
+                    }
+                }
+                fail("suspend function should throw an exception")
+            } catch (e: WebServiceException) {
+                // expected
+            } catch (e: Exception) {
+                fail("unexpected exception: $e")
+            }
+        }
+
+        testScheduler.advanceUntilIdle() // advance child coroutine to the end
+
+        // then: the retry virtual time equals the delay duration multipled by each retry attempt
+        val retryExecutionDuration = currentTime
+        val retryAttempts = maxAttempts - 1 // first attempt is not a retry
+        assertEquals(retryExecutionDuration, delayDuration.inWholeNanoseconds * retryAttempts)
+
+        // obs: since runTest is used in this test, the virtual time is used instead of real time as all delays
+        // are executed in the test context and consequently skipped.
+        // This is the reason why it can be equal to the delay duration and not slightly above.
+        // With the latter, an error margin would've been needed to consider the test successful
+        // (e.g. retryExecutionDuration.inWholeNanoseconds >= delayDuration.inWholeNanoseconds * maxAttempts +
+        // (delayDuration.inWholeNanoseconds * 0.01) // 1% error margin)
+        // more info at: https://github.com/Kotlin/kotlinx.coroutines/tree/master/kotlinx-coroutines-test
+
+        // and: the method was invoked the exact number of times specified in maxAttempts
+        coVerify {
+            remoteService.suspendCall()
+        }.wasInvoked(exactly = maxAttempts)
+    }
+
+    @Test
+    fun retryWithFaillingRetryPredicate() = runTest {
         // given: a retry configuration
         val maxAttempts = 3
         val delayDuration = 3.seconds
@@ -44,84 +116,33 @@ class RetryTests {
 
         // and: a remote service that always throws an exception
         coEvery { remoteService.suspendCall() }
-            .throws(WebServiceException("BAM!"))
+            .throws(RuntimeException("Surprise!"))
 
-        // when: an error margin is defined
-        val errorMargin = 0.10f // %
-
-        // and: a suspend function is executed with the retry instance
-        val retryExecutionDuration: Duration = measureTime {
+        launch {
             try {
-                // withContext(Dispatchers.Unconfined) { // used to disable delay skipping
-                    retry.executeSuspendFunction {
-                        remoteService.suspendCall()
-                    }
-                // }
-            } catch (e: WebServiceException) {
-                // ignore
+                // when: a decorated suspend function is executed with the retry instance
+                retry.executeSuspendFunction {
+                    remoteService.suspendCall()
+                }
+                fail("suspend function should throw an exception")
+            } catch (e: RuntimeException) {
+                // expected
             } catch (e: Exception) {
                 fail("Unexpected exception: $e")
             }
         }
 
-        // then: the retry time exceeds the delay duration multipled by each retry attempt but is within the error margin
-        println("Retry execution duration: ${retryExecutionDuration.inWholeNanoseconds}")
-        println("Delay duration: $delayDuration")
-        // assertTrue(
-            retryExecutionDuration.inWholeNanoseconds >=
-                    delayDuration.nanoseconds + (delayDuration.nanoseconds * errorMargin)
-        //)
+        testScheduler.advanceUntilIdle() // advance child coroutine to the end
 
-        // and: the method was invoked the exact number of possible retry attempts
+        // then: no delay is executed since the exception is not the one specified in the retry predicate
+        val retryExecutionDuration = currentTime
+        assertEquals(0, retryExecutionDuration)
+
+        // then: the method is invoked only once since there is no retry attempt
         coVerify {
             remoteService.suspendCall()
-        }.wasInvoked(exactly = maxAttempts - 1) // maxAttempts includes the first non-retry attempt
+        }.wasInvoked(exactly = once)
     }
 
-    @Test
-    fun exampleTest() = runTest {
-        val elapsed = measureTime {
-            val deferred = async {
-                delay(1000) // will be skipped
-                withContext(Dispatchers.Default) {
-                    delay(5000)
-                } // Dispatchers.Default doesn't know about TestCoroutineSchedule
-            }
-            deferred.await()
-        }
-        println(elapsed) // about five seconds
-    }
-
-    @Test
-    fun retryWithFaillingRetryIfPredicate() = runTest {
-        // given: a retry configuration
-        val maxAttempts = 3
-        val delayDuration = 3.seconds
-        val config: RetryConfig = retryConfig {
-            maxAttempts(maxAttempts)
-            retryIf { it is RuntimeException }
-            delay(delayDuration)
-        }
-
-        // and: a retry instance
-        val retry = Retry(config)
-
-        // and: a remote service that always throws an exception
-        coEvery { remoteService.suspendCall() }
-            .throws(WebServiceException("BAM!"))
-
-        // when: a decorated suspend function is executed with the retry instance
-        try {
-            retry.executeSuspendFunction {
-                remoteService.suspendCall()
-            }
-            fail("suspend function should throw an exception")
-        } catch (e: WebServiceException) {
-            // then: the method is invoked only once since there is no retry attempt
-            coVerify {
-                remoteService.suspendCall()
-            }.wasInvoked(exactly = once)
-        }
-    }
 }
 
