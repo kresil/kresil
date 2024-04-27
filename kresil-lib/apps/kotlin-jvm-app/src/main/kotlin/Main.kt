ea0a9b6295520db2ab118a553d81a8910ea078ab
Francisco Engenheiro <a49428@alunos.isel.pt>
2024-04-27 23:56:25 +0100

    🚀 feat(kotlin-jvm-app): adds small sample code to call library and check API visibility

--- kresil-lib/apps/kotlin-jvm-app/src/main/kotlin/Main.kt
+++ kresil-lib/apps/kotlin-jvm-app/src/main/kotlin/Main.kt
@@ -1,3 +1,49 @@
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kresil.retry.Retry
+import kresil.retry.builders.retryConfig
+import kresil.retry.config.RetryConfig
+import kotlin.time.Duration.Companion.milliseconds
+
+class WebServiceException : Exception()
+
+fun interface RemoteService {
+    suspend fun suspendCall()
+}
+
 fun main() {
-    println("Hello, Kresil!")
+    val remoteService = RemoteService {
+        throw WebServiceException()
+    }
+
+    val config: RetryConfig = retryConfig {
+        maxAttempts = 3 // includes the first non-retry currentAttempt
+        retryIf { it is WebServiceException }
+        constantDelay(500.milliseconds)
+    }
+
+    val retry = Retry(config) // or Retry() for default config
+    runBlocking {
+        launch {
+            // for all events
+            retry.onEvent { println(it) }
+            retry.onRetry { currAttempt ->
+                // handle retry event
+            }
+            retry.onError { error ->
+                // handle error event
+            }
+            // and more...
+        }
+
+        retry.executeSuspendFunction {
+            remoteService.suspendCall()
+        }
+        // or:
+        val decorated = retry.decorateSuspendFunction {
+            remoteService.suspendCall()
+        }
+        // and call it later: decorated()
+    }
+
 }
