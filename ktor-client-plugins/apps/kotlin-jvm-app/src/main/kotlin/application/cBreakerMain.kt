5367d067e401fb0a60fb57047fb59f0ec5d0e9a0
Francisco Engenheiro <a49428@alunos.isel.pt>
2024-06-05 21:15:52 +0100

    ♻️ refactor(cbreaker-plugin-app): server overload behaviour

--- ktor-client-plugins/apps/kotlin-jvm-app/src/main/kotlin/application/cBreakerMain.kt
+++ ktor-client-plugins/apps/kotlin-jvm-app/src/main/kotlin/application/cBreakerMain.kt
@@ -17,7 +17,9 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kresil.circuitbreaker.exceptions.CallNotPermittedException
 import kresil.ktor.client.plugins.circuitbreaker.KresilCircuitBreakerPlugin
+import kotlin.time.Duration.Companion.ZERO
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 private const val PORT = 8080
 
@@ -25,20 +27,22 @@ private suspend fun main() {
     val serverJob = CoroutineScope(Dispatchers.Default).launch { startUnreliableServer() }
     val client = HttpClient(CIO) {
         install(KresilCircuitBreakerPlugin) {
-            permittedNumberOfCallsInHalfOpenState = 2
-            noDelayInOpenState()
+            permittedNumberOfCallsInHalfOpenState = 1
+            maxWaitDurationInHalfOpenState = ZERO
+            constantDelayInOpenState(500.milliseconds)
             failureRateThreshold = 0.5
             slidingWindow(4, 4)
             recordFailureOnServerErrors()
         }
     }
 
-    repeat(10) {
+    repeat(25) {
         delay(250.milliseconds)
+        println("Client: sending request nr(${it + 1})")
         try {
             client.post {
                 url("http://127.0.0.1:$PORT/")
-                setBody("Hello, Kresil!")
+                setBody("Hello, from client!")
             }
         } catch (e: CallNotPermittedException) {
             println("Client received error: ${e.message}")
@@ -61,8 +65,13 @@ private suspend fun startUnreliableServer() {
                 println("Server received request nr(${requestCount}): $text")
                 when (requestCount) {
                     in 1..2 -> call.respondText("Server is back online!")
-                    in 3..6 -> call.respondText("Server is down", status = HttpStatusCode.InternalServerError)
-                    else -> call.respondText("Server is back online!")
+                    in 3..4 -> call.respondText("Server is down", status = HttpStatusCode.InternalServerError)
+                    else -> {
+                        println("Server is being overloaded")
+                        delay(2.seconds) // simulate response delay from being overloaded
+                        if (requestCount == 6) requestCount = 0 // simulate server recovery
+                        call.respondText("Server is down", status = HttpStatusCode.InternalServerError)
+                    }
                 }
             }
         }
