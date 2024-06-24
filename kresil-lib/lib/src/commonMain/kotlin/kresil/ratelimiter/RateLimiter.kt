072dd0d0c8f26082378e1bca1fcddc0838faebbb
Francisco Engenheiro <a49428@alunos.isel.pt>
2024-06-25 00:42:03 +0100

    🚀 feat(ratelimiter): adds naive implementation using a key-based semaphore

--- kresil-lib/lib/src/commonMain/kotlin/kresil/ratelimiter/RateLimiter.kt
+++ kresil-lib/lib/src/commonMain/kotlin/kresil/ratelimiter/RateLimiter.kt
@@ -1,10 +1,14 @@
 package kresil.ratelimiter
 
 import kresil.core.events.FlowEventListenerImpl
+import kresil.core.oper.Supplier
 import kresil.ratelimiter.config.RateLimiterConfig
-import kresil.ratelimiter.config.rateLimiterConfig
 import kresil.ratelimiter.config.defaultRateLimiterConfig
+import kresil.ratelimiter.config.rateLimiterConfig
 import kresil.ratelimiter.event.RateLimiterEvent
+import kresil.ratelimiter.semaphore.InMemoryKeyBasedSemaphore
+import kresil.ratelimiter.semaphore.KeyBasedSemaphore
+import kotlin.time.Duration
 
 /**
 * The [Rate Limiter](https://learn.microsoft.com/en-us/azure/architecture/patterns/rate-limiting) is a **proactive** resilience mechanism
@@ -13,8 +17,30 @@ A rate limiter is initialized with a configuration that, through pre-configured 
  * @param config The configuration for the rate limiter mechanism.
  * @see [rateLimiterConfig]
  */
-class RateLimiter(
+class RateLimiter<Key>(
     val config: RateLimiterConfig = defaultRateLimiterConfig(),
-) : FlowEventListenerImpl<RateLimiterEvent>() {
-    // TODO()
+    private val semaphore: KeyBasedSemaphore<Key> = InMemoryKeyBasedSemaphore(config)
+) : FlowEventListenerImpl<RateLimiterEvent>(), KeyBasedSemaphore<Key> by semaphore {
+
+    /**
+     * Decorates a suspending function with rate limiting.
+     * @param key The key to use for rate limiting.
+     * @param permits The number of permits required to execute the function.
+     * @param timeout The duration to wait for permits to be available.
+     * @param block The suspending function to decorate.
+     * @return The result of the suspending function.
+     */
+    suspend fun <R> call(
+        key: Key,
+        permits: Int,
+        timeout: Duration,
+        block: Supplier<R>
+    ): R {
+        acquire(key, permits, timeout)
+        try {
+            return block()
+        } finally {
+            release(key, permits)
+        }
+    }
 }
--- kresil-lib/lib/src/commonMain/kotlin/kresil/ratelimiter/exceptions/RateLimiterRejectedException.kt
+++ kresil-lib/lib/src/commonMain/kotlin/kresil/ratelimiter/exceptions/RateLimiterRejectedException.kt
@@ -2,6 +2,5 @@ package kresil.ratelimiter.exceptions
 
 /**
  * Exception that is thrown when a request is rejected by the rate limiter due the queue being full.
- * @param message The message to be displayed when the exception is thrown.
  */
-internal class RateLimiterRejectedException(message: String) : IllegalStateException(message)
+internal class RateLimiterRejectedException : IllegalStateException("Rate limiter has rejected the request")
